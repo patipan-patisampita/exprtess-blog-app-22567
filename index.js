f7593d9ea92985aa1a1bc80e28ec86a4ab2393d6
patipan-patisampita <patipan-patisampita@gmail.com>
2024-11-11 17:16:27 +0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import conectMongoDB from './config/db.js'
 import authRoute from './routes/authRoute.js'
+import postRoute from './routes/postRoute.js'
 import cookieParser from 'cookie-parser'
 import session from 'express-session'
 import flash from 'connect-flash';
+import path from 'path'
+
+import ConnectMongoDBSession from 'connect-mongodb-session'
+const MongoDBStore = ConnectMongoDBSession(session)
 
 const app = express()
 const port = process.env.PORT || 8080
@@ -14,6 +19,9 @@ conectMongoDB()
 // Middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// make uploads directory as static
+app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')))
 
 // Cookie middleware
 app.use(cookieParser(process.env.COOKIE_SECRET))
@@ -26,6 +34,10 @@ app.use(session({
     cookie: {
         maxAge: 60000 * 60 * 24 * 7 // 1 week
     },
+    store: new MongoDBStore({
+        uri: process.env.MONGO_DB_URI,
+        collection: 'sessions'
+    })
 }))
 
 // flash messages middleware
@@ -37,17 +49,20 @@ app.use(function (req, res, next) {
     next()
 })
 
+// store authenticated user's session data for views
+app.use(function (req, res, next) {
+    res.locals.user = req.session.user || null;
+    next();
+});
+
 //set view engine and view template
 app.set('view engine', 'ejs')
 
-//Home Page
-app.get('/', (req, res) => {
-    // res.json({ message: 'Hello from nodejs & Ecpress!' })
-    res.render('index', { title: 'Home Page' })
-})
-
-//Router
+// auth router
 app.use('/', authRoute)
+
+// post router
+app.use('/', postRoute)
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,36 +1,67 @@
 import express from 'express'
 import User from '../models/userModel.js'
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcryptjs'
+import nodemailer from 'nodemailer'
+import { guestRoute, protectedRoute } from '../middlewares/authMiddleware.js';
 
 const router = express.Router()
 
+// Looking to send emails in production? Check out our Email API/SMTP product!
+const transport = nodemailer.createTransport({
+    service: "Gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+        user: "patipan.patisampita@gmail.com",
+        pass: "pllx srrs fanw lfjb",
+    },
+})
+
+// const transport = nodemailer.createTransport({
+//     host: "sandbox.smtp.mailtrap.io",
+//     port: 2525,
+//     auth: {
+//         user: "36e8f252bf8cae",
+//         pass: "9247ed22b8f177"
+//     }
+// })
+
 //Route for Login page
-router.get('/login', (req, res) => {
-    res.render('login', { title: 'Login Page' })
+router.get('/login', guestRoute, (req, res) => {
+    res.render('login', { title: 'Login Page', active: 'login' })
 })
 
 //Route for Register page
-router.get('/register', (req, res) => {
-    res.render('register', { title: 'Register Page' })
+router.get('/register', guestRoute, (req, res) => {
+    res.render('register', { title: 'Register Page', active: 'register' })
 })
 
 //Route for Forgot password page
-router.get('/forgot-password', (req, res) => {
-    res.render('forgot-password', { title: 'Forgot-password Page' })
+router.get('/forgot-password', guestRoute, (req, res) => {
+    res.render('forgot-password', { title: 'Forgot-password Page', active: 'forgot' })
 })
 
 //Route for Reset password page
-router.get('/reset-password', (req, res) => {
-    res.render('reset-password', { title: 'Reset-password Page' })
+router.get('/reset-password/:token', guestRoute, async (req, res) => {
+    const { token } = req.params
+    const user = await User.findOne({ token })
+
+    if (!user) {
+        req.flash('error', 'Link expired or invalid!')
+        res.redirect('/forgot-password')
+    }
+
+    return res.render('reset-password', { title: 'Reset Password Page', active: 'reset', token })
 })
 
 // route for profile page
-router.get('/profile', (req, res) => {
+router.get('/profile', protectedRoute, (req, res) => {
     res.render('profile', { title: 'Profile Page', active: 'profile' })
 })
 
 //Route: Handle user Register Page
-router.post('/register', async (req, res) => {
+router.post('/register', guestRoute, async (req, res) => {
     const { name, email, password } = req.body
     try {
         const userExists = await User.findOne({ email });
@@ -59,7 +90,7 @@ router.post('/register', async (req, res) => {
 })
 
 // handle user login request
-router.post('/login', async (req, res) => {
+router.post('/login', guestRoute, async (req, res) => {
     const { email, password } = req.body
 
     try {
@@ -77,6 +108,82 @@ router.post('/login', async (req, res) => {
         console.error(error)
         req.flash('error', 'Something went wrong, try again!');
         res.redirect('/login')
+    }
+})
+
+// handle user logout
+router.post('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/login');
+})
+
+//handle forgot password post request
+router.post('/forgot-password', async (req, res) => {
+    const { email } = req.body
+    try {
+        const user = await User.findOne({ email })
+
+        if (!user) {
+            req.flash('error', 'User not found with this email!')
+            return res.redirect('/forgot-password')
+        }
+        const token = Math.random().toString(36).slice(2)
+
+        user.token = token
+        await user.save()
+
+        const info = await transport.sendMail({
+            from: '"DevChuchart" <patipan.patisampita@gmail.com>', // sender address
+            to: email, // list of receivers
+            subject: "Password Reset", // Subject line
+            text: "Reset your password!", // plain text body
+            html: `<p>Click this link to reset your password:<a href='http://localhost:3000/reset-password/${token}'>Reset Password</a><br>Thank you!</p>`, // html body
+        })
+
+        if (info.messageId) {
+            req.flash('success', 'Password reset link has been sent to your email!')
+            res.redirect('/forgot-password')
+        } else {
+            req.flash('error', 'Error sending email!')
+            res.redirect('/forgot-password')
+        }
+
+    } catch (error) {
+        console.error(error)
+        req.flash('error', 'Something went wrong, try again!');
+        res.redirect('/forgot-password')
+    }
+})
+
+//handle reset password post request
+router.post('/reset-password', async (req, res) => {
+    // console.log(req.body)
+    const { token, new_password, confirm_new_password } = req.body
+
+    try {
+        const user = await User.findOne({ token })
+
+        if (new_password !== confirm_new_password) {
+            req.flash('error', 'Password do not match!')
+            return res.redirect(`/reset-password/${token}`)
+        }
+
+        if (!user) {
+            req.flash('error', 'Invalid token!')
+            return res.redirect('/forgot-password')
+        }
+
+        user.password = await bcrypt.hash(new_password, 10)
+        user.token = null
+        await user.save()
+
+        req.flash('success', 'Password reset successfully!');
+        return res.redirect('/login')
+
+    } catch (error) {
+        console.error(error)
+        req.flash('error', 'Something went wrong, try again!');
+        return res.redirect('/reset-password')
     }
 })
 
